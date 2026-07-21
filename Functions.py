022d12da9d530a9d230b24ad2b1f45099e6a06b6
Mimi <mimi73727170@gmail.com>
2026-07-21 18:37:03 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,25 @@
+def greet() :
+    print("Hello Mimi")
+greet()
+greet()
+greet()
+
+def greet(name) :
+    print("Hello",name)
+greet("Mimi")
+greet("Soniya")
+
+def add(a,b) :
+    return a+b
+result = add(2,8)
+print(result)
+
+def square(num) :
+    return num*num
+print(square(5))
+
+def student(name,marks) :
+    print("Name :",name)
+    print("Marks :",marks)
+student("Mini",98)
+
